844bf76bc20a1c598d745118596224588162f2db
Sunil Rao <slysunil101@gmail.com>
2015-10-17 01:34:16 +0530

    Added basic jumping, ascii enemy template, heroine template

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,11 +3,17 @@ using System.Collections;
 
 public class Player : MonoBehaviour 
 {
+	[Header("Movement")]
 	public float movementForce;
 	public float maximumVelocity;
+	public float jumpForce;
+	//public float jumpingMultiplier;
+	//public float jumpingGravity;
 
 	private Rigidbody2D rigidbody;
 	private bool onAxis;
+
+	// TODO: Fix player janky ass jumping
 
 	// Use this for initialization
 	void Awake () 
@@ -17,25 +23,36 @@ public class Player : MonoBehaviour
 	}
 	
 	// Update is called once per frame
-	void Update () 
+	void FixedUpdate () 
 	{
 		Vector2 movementDirection = new Vector2();
 		movementDirection.x = Input.GetAxisRaw("Horizontal");
-		//movementDirection.y = Input.GetAxisRaw("Vertical");
+		movementDirection.y = 0;
+		movementDirection.Normalize();
 		movementDirection *= movementForce;
 
 		if (rigidbody.velocity.magnitude < maximumVelocity)
 		{
 			rigidbody.AddForce(movementDirection);
 		}
+
+		if (Input.GetKeyDown(KeyCode.Space) && onAxis)
+		{
+			movementDirection.y = jumpForce;
+			rigidbody.AddForce(movementDirection);
+		}
 	}
 
-	/*
+
 	void OnTriggerEnter2D(Collider2D other)
 	{
 		if (other.gameObject.name == "PlayerMovementAxis")
 		{
 			onAxis = true;
+
+			//Vector2 newVelocity = rigidbody.velocity;
+			//newVelocity.y = 0;
+			//rigidbody.velocity = newVelocity;
 		}
 	}
 
@@ -46,5 +63,4 @@ public class Player : MonoBehaviour
 			onAxis = false;
 		}
 	}
-	*/
 }
