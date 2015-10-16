22eccc56a2307fc52d81f9a0fad45196242f0b51
Sunil Rao <slysunil101@gmail.com>
2015-10-16 23:30:54 +0530

    Basic player movement, commiting to wireframe aesthetic

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour 
+{
+	public float movementForce;
+	public float maximumVelocity;
+
+	private Rigidbody2D rigidbody;
+	private bool onAxis;
+
+	// Use this for initialization
+	void Awake () 
+	{
+		// Retrive relevant vairables
+		rigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Vector2 movementDirection = new Vector2();
+		movementDirection.x = Input.GetAxisRaw("Horizontal");
+		//movementDirection.y = Input.GetAxisRaw("Vertical");
+		movementDirection *= movementForce;
+
+		if (rigidbody.velocity.magnitude < maximumVelocity)
+		{
+			rigidbody.AddForce(movementDirection);
+		}
+	}
+
+	/*
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.name == "PlayerMovementAxis")
+		{
+			onAxis = true;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.gameObject.name == "PlayerMovementAxis")
+		{
+			onAxis = false;
+		}
+	}
+	*/
+}
