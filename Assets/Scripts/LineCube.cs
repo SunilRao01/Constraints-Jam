4e6b5db00757134d26cfdd0ae02f4ccfffc6c1c0
Sunil Rao <slysunil101@gmail.com>
2015-10-17 15:47:14 +0530

    Added GL.Lines demo object in project as a stretch goal

---
+++ Assets/Scripts/LineCube.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LineCube : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("Main"), "time", 5.0,
+		                                      "easetype", iTween.EaseType.linear));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/RealTimeLines.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RealTimeLines : MonoBehaviour 
+{
+	public List<Vector3> vertexList;
+	public float lineDrawDelay;
+	
+	private int deleteCounter;
+	public int deleteTime;
+	public Color lineColor;
+	
+	void Start () 
+	{
+		vertexList.Add(transform.position);
+		
+		//StartCoroutine(waitThenVertex());
+	}
+	
+	void Update()
+	{
+		vertexList.Add(transform.position);
+		
+		deleteCounter++;
+		if (deleteCounter % deleteTime == 0)
+		{
+			vertexList.RemoveAt(0);
+		}
+	}
+
+	public void deleteLines()
+	{
+		vertexList.Clear();
+	}
+	
+	void OnRenderObject()
+	{
+		GL.Begin(GL.LINES);
+		GL.Color(lineColor);
+		{
+			for (int i = 0; i < vertexList.Count-1; i++)
+			{
+				GL.Vertex(vertexList[i]);
+				GL.Vertex(vertexList[i+1]);
+			}
+		}
+		GL.End();
+	}
+}
