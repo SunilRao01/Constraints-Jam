a9ea72feb138b9ef90ef77c201311c6560686aa7
Sunil Rao <slysunil101@gmail.com>
2015-11-18 01:26:29 +0530

    Added gameplay stages overhaul barebones

--- Assets/Scripts/EnemyASCIISpawner.cs
+++ Assets/Scripts/EnemyASCIISpawner.cs
@@ -15,6 +15,9 @@ public class EnemyASCIISpawner : MonoBehaviour
 	private char chosenLetter;
 	private bool spawning = true;
 
+	public bool phase1Path1;
+	public bool phase1Path2;
+
 	// Use this for initialization
 	void Start () 
 	{
@@ -36,11 +39,22 @@ public class EnemyASCIISpawner : MonoBehaviour
 			ascii.transform.SetParent(asciiParent.transform);
 			ascii.transform.localScale = new Vector3(1, 1, 1);
 
-			// Add force
-			Vector3 asciiShootPosition = new Vector3(Random.Range(-45, 45), -25, 0);
-			asciiShootPosition.Normalize();
-			asciiShootPosition *= asciiForce;
-			ascii.GetComponent<Rigidbody2D>().AddForce(asciiShootPosition);
+			if (phase1Path1)
+			{
+				// TODO: Add circular path to ascii 
+			}
+			else if (phase1Path2)
+			{
+				// TODO: Add lines and shoot characters really fast
+			}
+			else
+			{
+				// Add force
+				Vector3 asciiShootPosition = new Vector3(Random.Range(-45, 45), -25, 0);
+				asciiShootPosition.Normalize();
+				asciiShootPosition *= asciiForce;
+				ascii.GetComponent<Rigidbody2D>().AddForce(asciiShootPosition);
+			}
 
 			minSpawnInterval -= spawnIntervalScaling;
 			maxSpawnInterval -= spawnIntervalScaling;
